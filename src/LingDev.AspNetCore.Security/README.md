f9749f7658a2c8c094426be57ccfd80d5c5da37d
LingDev <lingjing0921@live.com>
2022-12-08 21:34:08 +0800

    Initialize code for LingDev.AspNetCore.Security

---
+++ src/LingDev.AspNetCore.Security/README.md
@@ -0,0 +1,4 @@
+# About
+
+`LingDev.AspNetCore.Security` A security authentication class library for AspNetCore web applications.
+This library provides permission-based and route-based authentication systems.
---
+++ src/LingDev.AspNetCore.Security/Route/PermissionRequirement.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace LingDev.AspNetCore.Security.Permission;
+
+/// <summary>
+/// Represents a route authorization requirement.
+/// </summary>
+internal class RouteRequirement : IAuthorizationRequirement
+{
+    /// <summary>
+    /// The route value.
+    /// </summary>
+    public string Value { get; set; }
+
+    /// <summary>
+    /// Initialize a <see cref="RouteRequirement"/>.
+    /// </summary>
+    /// <param name="route">The route value.</param>
+    public RouteRequirement(string route) => Value = route;
+}
